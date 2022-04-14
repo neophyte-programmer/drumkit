72311eba0b21d800d4f520dfae16228d3ca5452b
Nutifafa Attor <attorfafa@gmail.com>
2022-04-14 16:22:26 +0000

    created functions to initialise sound and keys

--- index.js
+++ index.js
@@ -3,44 +3,62 @@ var numberOfDrums = document.querySelectorAll('button').length;
 for(var i = 0; i < numberOfDrums; i++){
     document.querySelectorAll('button')[i].addEventListener("click", function () {
         buttonAnimation(this.textContent);
-        playSound(this.textContent);
+        initialiseKey(this.textContent);
     })
 }
 
 document.addEventListener('keypress', function(event) {
     buttonAnimation(event.key);
-    playSound(event.key);
+    initialiseKey(event.key);
 })
 
-function playSound(letter) {
+function playSound(audioURL) {
+    const audioObject = new Audio(audioURL);
+    audioObject.play();
+}
+
+function initialiseSound(elementSelector, audioURL) {
+    document.querySelector(elementSelector).addEventListener("click", () => {
+        playSound(audioURL);
+    })
+}
+
+function initialiseKey(letter) {
     switch(letter){
         case 'w':
-            var audio = new Audio('../sounds/crash.mp3');
-            audio.play(); 
+            // var audio = new Audio('../sounds/crash.mp3');
+            // audio.play(); 
+            initialiseSound('.w', '../sounds/crash.mp3');
         break;  
         case 'a' :
-            var audio = new Audio('../sounds/kick-bass.mp3');
-            audio.play();
+            // var audio = new Audio('../sounds/kick-bass.mp3');
+            // audio.play();
+            initialiseSound('.a', '../sounds/kick-bass.mp3');
         break;
         case 's' :
-            var audio = new Audio('../sounds/snare.mp3');
-            audio.play();
+            // var audio = new Audio('../sounds/snare.mp3');
+            // audio.play();
+            initialiseSound('.s', '../sounds/snare.mp3');
         break;
         case 'd' :
-            var audio = new Audio('../sounds/tom-1.mp3');
-            audio.play();
+            // var audio = new Audio('../sounds/tom-1.mp3');
+            // audio.play();
+            initialiseSound('.d', '../sounds/tom-1.mp3');
         break;
         case 'j' :
-            var audio = new Audio('../sounds/tom-2.mp3');
-            audio.play();
+            // var audio = new Audio('../sounds/tom-2.mp3');
+            // audio.play();
+            initialiseSound('.j', '../sounds/tom-2.mp3');
         break;
         case 'k' :
-            var audio = new Audio('../sounds/tom-3.mp3');
-            audio.play();
+            // var audio = new Audio('../sounds/tom-3.mp3');
+            // audio.play();
+            initialiseSound('.k', '../sounds/tom-3.mp3');
         break;
         case 'l' :
-            var audio = new Audio('../sounds/tom-4.mp3');
-            audio.play();
+            // var audio = new Audio('../sounds/tom-4.mp3');
+            // audio.play();
+            initialiseSound('.l', '../sounds/tom-4.mp3');
         break;
         default:
             console.log('button not found');
